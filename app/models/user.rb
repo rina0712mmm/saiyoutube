a1659253802a699c18983dc8afc7b6d01391a6e7
rina0712mmm <rina0712.mmm@gmail.com>
2020-11-19 19:31:52 +0900

    userモデル記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  
-end
+  has_many :videos
+
+  with_options presence: true do
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+    validates :company_name
+    validates :first_name, format: { with: NAME_REGEX }
+    validates :last_name, format: { with: NAME_REGEX }
+    validates :phone_number, format: { with: /\A\d{1,11}\z/ }
+  end
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
